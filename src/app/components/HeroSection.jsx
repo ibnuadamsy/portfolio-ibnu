d002359ec0bbbda4cde202f9c16422ebbf4000fb
danabijakqa <it.support@danabijak.com>
2024-03-18 09:12:54 +0700

    update cv on resume

--- src/app/components/HeroSection.jsx
+++ src/app/components/HeroSection.jsx
@@ -42,7 +42,7 @@ const HeroSection = () => {
               Reach Me
             </Link>
             <Link
-              href="https://drive.google.com/file/d/1E7Jq9a0S85rCrZgJJiDOGiLe7Iekx4zx/view?usp=sharing"
+              href="https://drive.google.com/file/d/1fciZV2cZGINIkMD1faJ0hcaFw-R5Pgj6/view?usp=sharing"
               className="px-1 inline-block py-1 w-full sm:w-fit rounded-full bg-gradient-to-br from-primary-500 to-secondary-500 hover:bg-slate-800 text-white mt-3"
             >
               <span className="block bg-[#121212] hover:bg-slate-800 rounded-full px-5 py-2">
